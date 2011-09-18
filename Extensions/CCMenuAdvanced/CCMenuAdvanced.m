2e8084944d239cbc57261efa9780e23a4f906c8f
Stepan Generalov <psi.pk.ru@gmail.com>
2011-09-19 00:39:54 +0400

    CCMenuAdvanced: add CCMenuItem's itemForTouch:

--- Extensions/CCMenuAdvanced/CCMenuAdvanced.m
+++ Extensions/CCMenuAdvanced/CCMenuAdvanced.m
@@ -614,5 +614,25 @@
 
 #endif
 
+-(CCMenuItem *) itemForTouch: (UITouch *) touch
+{
+	CGPoint touchLocation = [touch locationInView: [touch view]];
+	touchLocation = [[CCDirector sharedDirector] convertToGL: touchLocation];
+	
+	CCMenuItem* item;
+	CCARRAY_FOREACH(children_, item){
+		// ignore invisible and disabled items: issue #779, #866
+		if ( [item visible] && [item isEnabled] ) {
+			
+			CGPoint local = [item convertToNodeSpace:touchLocation];
+			CGRect r = [item rect];
+			r.origin = CGPointZero;
+			
+			if( CGRectContainsPoint( r, local ) )
+				return item;
+		}
+	}
+	return nil;
+}
 @end
 
